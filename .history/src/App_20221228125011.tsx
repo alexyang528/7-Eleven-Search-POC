086652900a267618214ac37b2ec84d7b75fde2b4
rkeerthi <sunnykeerthi@gmail.com>
2022-12-28 12:53:47 +0530

    added analytics sdk

--- .history/src/App_20221228125011.tsx
+++ .history/src/App_20221228125011.tsx
@@ -7,39 +7,33 @@ import {
   VerticalResults,
   FocusedItemData,
   DropdownItem,
-  RenderEntityPreviews,
+  RenderEntityPreviews
 } from "@yext/search-ui-react";
 import {
   provideHeadless,
   VerticalResults as VerticalResultsData,
   Result,
-  useSearchState,
+  useSearchState
 } from "@yext/search-headless-react";
 import { ProductCard } from "./components/cards/ProductCard";
 import { Product } from "./types/products";
 import classnames from "classnames";
-import { provideSearchAnalytics } from "@yext/analytics"; // can also be imported as provideAnalytics
-import { useProdContext } from "./context/prodContext";
-import { useEffect } from "react";
+
 
 const entityPreviewSearcher = provideHeadless({
   apiKey: "184b8f65a7921212f4a09118718f3db9",
   experienceKey: "7-eleven-search-poc",
   locale: "en",
   headlessId: "entity-preview-searcher",
-  additionalQueryParams: { limit: '{"products": 4}' },
+  additionalQueryParams: {"limit": '{"products": 4}'}
 });
 
-const searchAnalytics = provideSearchAnalytics({
-  experienceKey: "7-eleven-search-poc",
-  experienceVersion: "PRODUCTION",
-  businessId: 3878450, // this comes from the url of your Yext account
-});
 
 const App = (): JSX.Element => {
-  const query = useSearchState((state) => state.query.input);
-  const { isProdDetail, setIsProdDetail } = useProdContext();
-  const renderEntityPreviews: RenderEntityPreviews = (
+
+  const query = useSearchState(state => state.query.input)
+
+  const renderEntityPreviews : RenderEntityPreviews = (
     autocompleteLoading,
     verticalKeyToResults: Record<string, VerticalResultsData>,
     dropdownItemProps: {
@@ -51,24 +45,26 @@ const App = (): JSX.Element => {
       ariaLabel: (value: string) => string;
     }
   ): JSX.Element | null => {
+    
     if (!query) return null;
 
-    const productResults = verticalKeyToResults["products"]
-      ?.results as unknown as Result<Product>[];
+    const productResults = verticalKeyToResults["products"]?.results as unknown as Result<Product>[];
 
     return productResults ? (
       <div
+        // laying out the product previews in a grid
         className={classnames("grid grid-cols-4 px-8 gap-8", {
+          // fading the results if they're loading
           "opacity-50": autocompleteLoading,
         })}
       >
         {productResults.map((result, i) => (
+          // DropdownItem is impored from @yext/search-ui-react
           <DropdownItem
             key={result.rawData.id}
             value={result.rawData.name}
-            onClick={() =>
-              history.pushState(null, "", `/product/${result.rawData.id}`)
-            }
+            // when an item is clicked, it will change the URL
+            onClick={() => history.pushState(null, "", `/product/${result.rawData.id}`)}
             ariaLabel={dropdownItemProps.ariaLabel}
           >
             <ProductCard result={result} />
@@ -77,44 +73,25 @@ const App = (): JSX.Element => {
       </div>
     ) : null;
   };
-  useEffect(() => {
-    !isProdDetail && setIsProdDetail(false);
-  });
+
   return (
     <div className="flex justify-center px-4 py-6">
-      <div className="w-full max-w-7xl home">
-        {!isProdDetail && (
-          <SearchBar
-            visualAutocompleteConfig={{
+      <div className="w-full max-w-7xl">
+        <SearchBar 
+          visualAutocompleteConfig={{
               entityPreviewSearcher: entityPreviewSearcher,
               includedVerticals: ["products"],
               renderEntityPreviews: renderEntityPreviews,
               universalLimit: { products: 4 },
               entityPreviewsDebouncingTime: 500,
-            }}
-          />
-        )}
+          }}
+        />
         <div className="flex gap-16 pb-8">
-          <StandardFacets
-            customCssClasses={{
-              standardFacetsContainer: "min-w-[200px]  hidden md:block",
-            }}
-          />
+          <StandardFacets customCssClasses={{ standardFacetsContainer: "min-w-[200px]" }} />
           <div className="flex-col">
-            <ResultsCount customCssClasses={{ resultsCountContainer: "" }} />
-            <AppliedFilters
-              customCssClasses={{
-                clearAllButton: "hidden",
-                appliedFiltersContainer: "pb-4",
-              }}
-            />
-            <VerticalResults
-              CardComponent={ProductCard}
-              customCssClasses={{
-                verticalResultsContainer:
-                  "grid grid-cols-1 md:grid-cols-4 md:gap-4 md:pb-4",
-              }}
-            />
+            <ResultsCount customCssClasses={{ resultsCountContainer:"" }}/>
+            <AppliedFilters customCssClasses={{ clearAllButton:"hidden", appliedFiltersContainer:"pb-4" }}/>
+            <VerticalResults CardComponent={ProductCard} customCssClasses={{ verticalResultsContainer:"grid grid-cols-4 gap-4 pb-4" }}/>
             <Pagination />
           </div>
         </div>
